c85bdeffa60717b8f4d74529a8ab78e8f9438adb
RezaR2P <kecilreza575@gmail.com>
2026-06-14 09:05:05 +0700

    fix: update logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/LOGO YEN.png';
+import logo from '../assets/logo.png';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
